960ba3c8ab9913291d820c35149307c87a9fd7f3
salsbiila <13522062@std.stei.itb.ac.id>
2024-10-22 09:14:08 +0700

    style: fixing title

--- php/src/app/views/company-edit-profile/index.php
+++ php/src/app/views/company-edit-profile/index.php
@@ -22,7 +22,7 @@ $home = false;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Company Profile</title>
+    <title>Company Edit Profile</title>
     <link rel="stylesheet" href="../../../public/css/navbar/style.css">
     <link rel="stylesheet" href="../../../public/css/company-edit-profile/index.css">
     <link rel="stylesheet" href="../../../public/css/footer/style.css">
